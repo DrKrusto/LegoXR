7c67f14225ecfc6164ecceb9cbb19030d673ddd8
Evan <evan.roussin@gmail.com>
2022-12-07 14:55:12 +0100

    Ajusted snap points highlight

--- EvanTest/Assets/Lego/Scripts/BrickSnapping.cs
+++ EvanTest/Assets/Lego/Scripts/BrickSnapping.cs
@@ -12,14 +12,14 @@ public class BrickSnapping : MonoBehaviour
     public List<GameObject> snapPoints;
 
     [SerializeField]
-    public float snapRadius = 0.1f;
+    public float snapRadius;
     [SerializeField]
     public Mesh snapHighlightMesh;
     [SerializeField]
     public Material snapHighlightMaterial;
 
     private Grabbable grabComponent;
-    private List<Tuple<GameObject, GameObject>> snapPointsColliding = new List<Tuple<GameObject, GameObject>>();
+    private List<Tuple<SnapPoint, SnapPoint>> snapPointsColliding = new();
 
     private void Start()
     {
@@ -29,35 +29,27 @@ public class BrickSnapping : MonoBehaviour
 
     void Update()
     {
-        VerifyIfIsHeld();
-        if (!grabComponent.BeingHeld)
-        {
-            snapPointsColliding.Clear();
-        }
-        if (snapPointsColliding.Count > 0)
-        {
-            //Snap();
-        }
+        HoldingBrick();
     }
 
-    void Snap(List<Tuple<Transform, Transform>> snapPoints)
+    void Snap()
     {
-        if (snapPoints.Count > 0) return;
-        if (snapPoints.Count == 1)
+        if (snapPointsColliding.Count > 0) return;
+        if (snapPointsColliding.Count == 1)
         {
             // TODO
             return;
         }
 
-        foreach (var pairOfSnapPoints in snapPoints)
+        foreach (var pairOfSnapPoints in snapPointsColliding)
         {
 
         }
 
-        Transform firstCubeSnapPoint1 = snapPoints[0].Item1;
-        Transform firstCubeSnapPoint2 = snapPoints[1].Item1;
-        Transform secondCubeSnapPoint1 = snapPoints[0].Item2;
-        Transform secondCubeSnapPoint2 = snapPoints[1].Item2;
+        Transform firstCubeSnapPoint1 = snapPointsColliding[0].Item1.transform;
+        Transform firstCubeSnapPoint2 = snapPointsColliding[1].Item1.transform;
+        Transform secondCubeSnapPoint1 = snapPointsColliding[0].Item2.transform;
+        Transform secondCubeSnapPoint2 = snapPointsColliding[1].Item2.transform;
         transform.position = secondCubeSnapPoint1.position + (transform.position - firstCubeSnapPoint1.position);
         transform.rotation = secondCubeSnapPoint1.GetComponentInParent<BrickSnapping>().transform.rotation;
     }
@@ -68,77 +60,31 @@ public class BrickSnapping : MonoBehaviour
         foreach (var snapPoint in snapPoints)
         {
             var snapPointController = snapPoint.AddComponent<SnapPoint>();
-            snapPointController.ParameterSnapPoint(snapHighlightMesh, snapHighlightMaterial, snapRadius);
+            snapPointController.ParameterSnapPoint(transform, snapHighlightMesh, snapHighlightMaterial, snapRadius);
         }
     }
 
-    void CreateSphereMesh()
-    {
-        var meshBuilder = new MeshBuilder();
-
-        // Set the number of segments and rings for the sphere
-        const int segments = 64;
-        const int rings = 32;
-
-        // Create the vertices and normals for the sphere
-        for (int y = 0; y <= rings; y++)
-        {
-            float v = y / (float)rings;
-            float theta = v * Mathf.PI;
-
-            for (int x = 0; x <= segments; x++)
-            {
-                float u = x / (float)segments;
-                float phi = u * Mathf.PI * 2;
-
-                Vector3 vertex = new Vector3(
-                    radius * Mathf.Sin(theta) * Mathf.Cos(phi),
-                    radius * Mathf.Cos(theta),
-                    radius * Mathf.Sin(theta) * Mathf.Sin(phi)
-                );
-                Vector3 normal = (vertex - Vector3.zero).normalized;
-
-                meshBuilder.Vertices.Add(vertex);
-                meshBuilder.Normals.Add(normal);
-            }
-        }
-        // Create the triangles for the sphere
-        for (int y = 0; y < rings; y++)
-        {
-            for (int x = 0; x < segments; x++)
-            {
-                int v0 = y * (segments + 1) + x;
-                int v1 = v0 + segments + 1;
-                int v2 = v0 + 1;
-                int v3 = v1 + 1;
-
-                meshBuilder.AddTriangle(v0, v1, v2);
-                meshBuilder.AddTriangle(v2, v1, v3);
-            }
-        }
-
-        // Create the mesh
-        var mesh = meshBuilder.CreateMesh();
-        mesh.name = "Sphere";
-
-        return mesh;
-    }
-
-    void VerifyIfIsHeld()
+    void HoldingBrick()
     {
         if (grabComponent.BeingHeld)
         {
             snapPointsColliding.Clear();
-            foreach (var snapPoint in snapPoints)
+            foreach (var snapPoint in snapPoints.Select(r => r.GetComponent<SnapPoint>()))
             {
-                Collider[] collidedSnapPoints = Physics.OverlapSphere(snapPoint.transform.position, snapRadius)
-                    .Where(r => r.tag == "SnapPoint" && r.gameObject != snapPoint.gameObject)
-                    .ToArray();
-                var collider = collidedSnapPoints.FirstOrDefault();
-                if (!collider) continue;
-                snapPointsColliding.Add(new Tuple<GameObject, GameObject>(snapPoint, collider.gameObject));
-                collider.GetComponent<SnapPoint>().IsCollidingWithSnapPoint = true;
-                snapPoint.GetComponent<SnapPoint>().IsCollidingWithSnapPoint = true;
+                var collidedSnapPoint = Physics.OverlapSphere(snapPoint.transform.position, snapRadius)
+                    .Where(r => r.tag == "SnapPoint" && r.gameObject != snapPoint.gameObject && r.transform.parent != transform.parent)
+                    .Select(r => r.GetComponent<SnapPoint>())
+                    .FirstOrDefault();
+                if (!collidedSnapPoint) continue;
+                snapPoint.SnapToSnapPoint(collidedSnapPoint);
+                snapPointsColliding.Add(new Tuple<SnapPoint, SnapPoint>(snapPoint, collidedSnapPoint));
+            }
+        }
+        else
+        {
+            if (snapPointsColliding.Count > 0)
+            {
+                //Snap();
             }
         }
     }
--- EvanTest/Assets/Lego/Scripts/SnapPoint.cs
+++ EvanTest/Assets/Lego/Scripts/SnapPoint.cs
@@ -1,5 +1,7 @@
+using BNG;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SnapPoint : MonoBehaviour
@@ -7,26 +9,66 @@ public class SnapPoint : MonoBehaviour
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
     private SphereCollider sphereCollider;
-    private bool isCollidingWithSnapPoint;
+    private Transform parentBrick;
+    private float snapRadius = .1f;
+    protected SnapPoint snappedTo;
 
-    public bool IsCollidingWithSnapPoint { 
-        get { return isCollidingWithSnapPoint; } 
-        set
-        {
-            isCollidingWithSnapPoint = value;
-            meshRenderer.enabled = value;
-        } 
+    private void Update()
+    {
+        var collider = Physics.OverlapSphere(transform.position, snapRadius).Where(r => CanBeSnapped(r)).FirstOrDefault();
+        meshRenderer.enabled = CanBeSnapped(collider);
     }
 
-    public void ParameterSnapPoint(Mesh mesh, Material material, float snapRadius)
+    public void ParameterSnapPoint(Transform parent, Mesh mesh, Material material, float snapRadius)
     {
         meshFilter = gameObject.AddComponent<MeshFilter>();
         meshRenderer = gameObject.AddComponent<MeshRenderer>();
         sphereCollider = gameObject.AddComponent<SphereCollider>();
         sphereCollider.isTrigger = true;
-        IsCollidingWithSnapPoint = true;
-        meshFilter.mesh = mesh; // should do procedurally according to radius!
+        meshFilter.mesh = mesh;
         meshRenderer.material = material;
-        sphereCollider.radius = snapRadius;
+        meshRenderer.enabled = false;
+        sphereCollider.radius = 1;
+        parentBrick = parent;
+        transform.localScale *= snapRadius;
+    }
+
+    public void SnapToSnapPoint(SnapPoint snapPoint)
+    {
+        snappedTo = snapPoint;
+        snapPoint.snappedTo = this;
+    }
+
+    public void UnSnap()
+    {
+        snappedTo.snappedTo = null;
+        snappedTo = null;
+    }
+
+    private bool CanBeSnapped(Collider other)
+    {
+        try
+        {
+            var otherParent = other.gameObject.GetComponentInParent<SnapPoint>().parentBrick;
+            var beingHeld = otherParent.GetComponent<Grabbable>().BeingHeld || parentBrick.GetComponent<Grabbable>().BeingHeld;
+            var isNotSameCollider = other.transform != transform;
+            var isSnapPoint = other.tag == "SnapPoint";
+            var isNotFromSameBrick = otherParent != parentBrick;
+            return beingHeld && isNotSameCollider && isSnapPoint && isNotFromSameBrick;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (CanBeSnapped(other)) meshRenderer.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (CanBeSnapped(other)) meshRenderer.enabled = false;
     }
 }
